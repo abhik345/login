e831a9933822501bda2d8951c55d3c869728536f
abhik345 <abhikdas345@gmail.com>
2023-11-10 07:09:39 +0530

    Login user created backend nodejs

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config;
+
+const express = require("express");
+
+const app = express();
+
+const userRouter = require("./api/users/user.router");
+app.use(express.json());
+
+app.use("/api/users", userRouter);
+
+// app.get("/api", (req, res) => {
+//   res.json({
+//     success: 1,
+//     message: "This is rest apis working",
+//   });
+// });
+
+app.listen(process.env.APP_PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("server up and running");
+  }
+});
---
+++ config/database.js
@@ -0,0 +1,22 @@
+const { createPool } = require('mysql')
+
+// const pool = createPool({
+//     port : 3306,
+//     host : "localhost",
+//     user : "root",
+//     password :"",
+//     database : "test",
+//     connectionLimit : 10
+// })
+
+const pool = createPool({
+    port : process.env.APP_PORT,
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database : process.env.MYSQL_DB,
+    connectionLimit : 10
+})
+
+
+module.exports = pool;
